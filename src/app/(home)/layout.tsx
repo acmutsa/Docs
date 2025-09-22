52ec2c58bbbabcb9da8c7235122ecff91e6d2947
adanrsantos <adanrsantos2016@gmail.com>
2025-09-22 01:28:18 -0500

    init

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,5 +2,18 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+  return (
+    <HomeLayout 
+      {...baseOptions()}
+      links={[
+        {
+          text: 'Documentation',
+          url: '/docs',
+          active: 'nested-url'
+        },
+      ]}
+    >
+      {children}
+    </HomeLayout>
+  );
 }
--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,4 +1,6 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
+import Logo from '@/public/acm_oss_logo.png'
 
 /**
  * Shared layout configurations
@@ -7,24 +9,31 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Home Layout: app/(home)/layout.tsx
  * Docs Layout: app/docs/layout.tsx
  */
+
+export const logo = (
+  <>
+    <Image
+      alt="ACM Projects"
+      src={Logo}
+      sizes="100px"
+      className="w-6"
+      aira-label="ACM Projects"
+    />
+  </>
+)
+
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          My App
+          {logo}
+          ACM OSS
         </>
       ),
     },
     // see https://fumadocs.dev/docs/ui/navigation/links
     links: [],
+    githubUrl: 'https://github.com/',
   };
 }
