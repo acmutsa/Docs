959d2f8246c3a26e68dc4075a2c422e30f737d43
adanrsantos <adanrsantos2016@gmail.com>
2025-09-22 16:30:24 -0500

    finally get icons to work

---
+++ src/components/icon.tsx
@@ -0,0 +1,20 @@
+// icon.tsx
+import type { LucideIcon } from 'lucide-react';
+import { TerminalIcon } from 'lucide-react';
+import { type HTMLAttributes } from 'react';
+
+export default function IconContainer({
+  icon: Icon,
+  ...props
+}: HTMLAttributes<HTMLDivElement> & {
+  icon?: LucideIcon;
+}): React.ReactElement {
+  return (
+    <div
+      {...props}
+      className={props.className}
+    >
+      {Icon ? <Icon /> : <TerminalIcon />}
+    </div>
+  );
+}
--- src/lib/source.ts
+++ src/lib/source.ts
@@ -2,6 +2,7 @@ import { docs } from '@/.source';
 import { loader } from 'fumadocs-core/source';
 import { icons } from 'lucide-react';
 import { createElement } from 'react';
+import IconContainer from '@/components/icon';
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
@@ -9,9 +10,9 @@ export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
   icon(icon) {
-    if (!icon) {
-      return;
-    }
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-  },
+    if (icon && icon in icons)
+      return createElement(IconContainer, {
+        icon: icons[icon as keyof typeof icons],
+      });
+  }
 });
