b2838278d9b35f6ae8b13a1e537fdc1f1d092a3a
joshuasilva414 <joshuasilva414@gmail.com>
2025-12-04 16:49:50 -0600

    hk getting started

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
-import '@/app/global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import "@/app/global.css";
+import { Banner } from "@/components/banner";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export const metadata = {
-  title: 'ACM Projects',
-  description: 'ACM Projects Documentation',
-}
+  title: "ACM Projects",
+  description: "ACM Projects Documentation",
+};
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <Banner>Caution 🚧: This site is under construction</Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
